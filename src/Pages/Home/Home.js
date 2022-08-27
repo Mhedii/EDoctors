6cff035c9d789d91054e88579c465f81360f2653
Mhedii <mehedi1234.mh@gmail.com>
2022-08-27 13:13:03 +0600

    Navbar

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Banner from './Banner/Banner.js';
+import Infos from './Info/Infos.js';
+
+const Home = () => {
+    return (
+        <div>
+            <Banner></Banner>
+            <Infos></Infos>
+        </div>
+    );
+};
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'banner-bg': "url('/src/images/bg.png')",
+
+      }
+    },
+  },
+
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#0FCFEC",
+          secondary: "#19D3AE",
+          accent: "#3A4256",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
